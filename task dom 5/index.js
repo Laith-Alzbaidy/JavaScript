224889efb679e139460494c261ff676ab13a9068
laith-alzbaidy <laithalzbaidy@gmail.com>
2023-05-07 14:38:42 +0300

    update

---
+++ task dom 5/index.js
@@ -0,0 +1,24 @@
+let section = document.querySelector('section')
+let card = document.querySelectorAll('.card')
+
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener('click', function () {
+    card[i].classList.toggle('cardd')
+  })
+}
+
+window.onload = shuffle()
+function shuffle() {
+  let arr = []
+  for (let i = 0; i < card.length; i++) {
+    let ran = Math.floor(Math.random() * card.length)
+    section.append(card[ran])
+    console.log(card[ran])
+    while (arr.includes(ran)) {
+      ran = Math.floor(Math.random() * card.length)
+    }
+    arr.push(ran)
+  }
+}
+
+
